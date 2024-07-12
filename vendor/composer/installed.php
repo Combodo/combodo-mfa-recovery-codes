1cb9a604fbb35fc0efe3d0ed90dc051b82dabf61
Eric Espie <eric.espie@combodo.com>
2024-07-12 15:21:45 +0200

    Modules creation

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'combodo/combodo-mfa-recovery-codes',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => 'b404c819f28f197e040860eed7aca478b5554958',
+        'type' => 'itop-extension',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'combodo/combodo-mfa-recovery-codes' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'b404c819f28f197e040860eed7aca478b5554958',
+            'type' => 'itop-extension',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
