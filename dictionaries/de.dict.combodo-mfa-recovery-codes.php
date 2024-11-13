4bf5360310f17f52e30818e1baabc5f985f11e85
Lars Kaltefleiter <lars@kaltefleiter.org>
2024-11-13 13:26:00 +0100

    :globe_with_meridians: German Translations (#1)
    * German translation

---
+++ dictionaries/de.dict.combodo-mfa-recovery-codes.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Localized data
+ *
+ * @copyright   Copyright (C) 2024 Combodo SAS
+ * @license     http://opensource.org/licenses/AGPL-3.0
+ * @author      Lars Kaltefleiter <lars.kaltefleiter@itomig.de>
+ */
+
+Dict::Add('DE DE', 'German', 'Deutsch', array(
+	'combodo-mfa-recovery-codes/Operation:MFARecoveryCodesView/Title' => 'Recovery-Codes anzeigen',
+	'MFA:MFAUserSettingsRecoveryCodes:Description' => 'Bietet eine Liste von 10 einmal verwendbaren Recovery-Codes, um auf Ihr Konto zuzugreifen, wenn Sie Zugriff auf Ihr Gerät verlieren',
+	'MFA:login:switch:label:MFAUserSettingsRecoveryCodes' => 'Recovery-Codes verwenden',
+	'MFA:RC:CodeValidation:Title' => 'Recovery-Codes eingeben',
+	'MFA:RC:EnterCode' => 'Recovery-Codes',
+	'MFA:RC:Config:Title' => 'MFA-Recovery-Codes',
+	'MFA:RC:Config:Warning' => 'Behalten Sie Ihre Recovery-Codes so sicher wie Ihr Passwort. Wir empfehlen, sie mit eine Passwortdatenbank zu speichern',
+	'MFA:RC:Settings:Title' => 'Einstellungen',
+	'MFA:RC:Settings:Code:label' => 'Liste der Recovery-Codes',
+	'MFA:RC:Copy' => 'Recovery-Codes in die Zwischenablage kopieren',
+	'MFA:RC:Copy:Done' => 'Recovery-Codes wurden in die Zwischenablage kopiert',
+	'MFA:RC:RebuildCodes' => 'Neue Recovery-Codes generieren',
+	'MFA:RC:RebuildCodes+' => 'Wenn neue Recovery-Codes generiert werden, werden die alten nicht mehr gültig sein.',
+	'Class:MFAUserSettingsRecoveryCodes' => 'Recovery-Codes',
+	'Class:MFAUserSettingsRecoveryCodes/Attribute:mfarecoverycodes_list' => 'Liste der Recovery-Codes',
+	'Class:MFARecoveryCode' => 'Recovery-Code',
+	'Class:MFARecoveryCode/Attribute:code' => 'Code',
+	'Class:MFARecoveryCode/Attribute:status' => 'Status',
+	'Class:MFARecoveryCode/Attribute:mfausersettingsrecoverycodes_id' => 'Benutzereinstellungen',
+
+));
